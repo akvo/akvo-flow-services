97e24cbe3c35bc7d211f19446c3f318e043934bd
Jonas Enlund <jonas.enlund@gmail.com>
2015-04-10 15:48:03 +0300

    [#91] Updated flow-services version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flow-services "0.9.4"
+(defproject flow-services "0.9.5"
   :description "HTTP layer to applets functionality"
   :url "https://github.com/akvo/akvo-flow-services"
   :license {:name "GNU Affero General Public License"
@@ -29,7 +29,7 @@
                  [com.google.gdata/core "1.47.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  ; Akvo FLOW dependencies
-                 [exporterapplet "1.6.0"]
+                 [exporterapplet "1.8.3"]
                  [org.json/json "20090211"]
                  [log4j/log4j "1.2.16"]
                  ; Configuration and Stats
