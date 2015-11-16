3e2de082375f7b1ba3bb3d3b42ed5a0d9f76ee16
Jonas Enlund <jonas.enlund@gmail.com>
2015-11-16 11:29:38 +0200

    [#122] Add jackson dep to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flow-services "0.9.9"
+(defproject flow-services "0.9.10-SNAPSHOT"
   :description "HTTP layer to applets functionality"
   :url "https://github.com/akvo/akvo-flow-services"
   :license {:name "GNU Affero General Public License"
@@ -30,9 +30,10 @@
                  [com.google.gdata/core "1.47.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  ; Akvo FLOW dependencies
-                 [exporterapplet "1.9.2"]
+                 [exporterapplet "1.9.3"]
                  [org.json/json "20090211"]
                  [log4j/log4j "1.2.16"]
+                 [org.codehaus.jackson/jackson-mapper-asl "1.9.13"]
                  ; Configuration and Stats
                  [com.google.appengine/appengine-tools-sdk "1.9.18"]
                  [com.google.appengine/appengine-remote-api "1.9.18"]
