1d1f24998c7f9e9bea8efcda2083a5dbbbfff0f0
Dan Lebrero <dlebrero@gmail.com>
2018-03-28 11:01:49 +0200

    [#174] Creating a local git repo with the flow config folder

--- dev/start-dev-env.sh
+++ dev/start-dev-env.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ ! -d "/akvo-flow-server-config/" ]; then
-    echo "Creating test git repo with Flow config"
-    mkdir -p /server-config-repo
-    cp -r flow-server-config /server-config-repo
+    echo "Creating fake git repo with Flow config"
+    cp -r dev/flow-server-config /server-config-repo
     pushd /server-config-repo
     git init
-    git commit -a -m "Initial commit"
+    git config --global user.email "you@example.com"
+    git config --global user.name "Your Name"
+    git add -A
+    git commit -m "Initial commit"
     git clone /server-config-repo /akvo-flow-server-config
     popd
 fi
