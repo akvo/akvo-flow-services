0dfaf5a86accc1d5a68289fa2d087320575d6cb5
Dan Lebrero <dlebrero@gmail.com>
2018-03-29 11:47:34 +0200

    [#174] Special case for development servers

--- src/akvo/flow_services/util.clj
+++ src/akvo/flow_services/util.clj
@@ -3,7 +3,10 @@
 
 (defn datastore-spec [app-id-or-bucket]
   (let [cfg (config/find-config app-id-or-bucket)]
-    {:hostname (:domain cfg)
-     :service-account-id (:service-account-id cfg)
-     :private-key-file (:private-key-file cfg)
-     :port 443}))
+    (if (= "flowservices-dev-config" (:app-id cfg))
+      {:hostname           "localhost"
+       :port               8888}
+      {:hostname           (:domain cfg)
+       :service-account-id (:service-account-id cfg)
+       :private-key-file   (:private-key-file cfg)
+       :port               443})))
