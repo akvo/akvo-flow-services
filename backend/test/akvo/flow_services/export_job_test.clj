18f1b2d01b0b295c296af155373d07701c156fff
Dan Lebrero <dlebrero@gmail.com>
2018-04-30 17:30:53 +0200

    [#172] removing dup from tests

--- backend/test/akvo/flow_services/export_job_test.clj
+++ backend/test/akvo/flow_services/export_job_test.clj
@@ -8,68 +8,36 @@
   (is (not (scheduler/gdpr-flow? {"opts" {}})))
   (is (not (scheduler/gdpr-flow? {"opts" {"gdpr" "false"}})))
 
-  (is (= (scheduler/create-report-in-flow {"opts"       {"email" "user@akvo.org"}
-                                           "baseURL"    "http://foobar"
-                                           "exportType" "GEOSHAPE"})
-         {:method :post
-          :url    "http://foobar/reports"
-          :body   {:report {:state      "IN_PROGRESS"
-                            :user       "user@akvo.org"
-                            :reportType "GEOSHAPE"}}}))
-
   (testing "flow notifications"
     (testing "create request"
-      (is (= (scheduler/finish-report-in-flow {"opts"       {"email" "user@akvo.org"}
+      (is (= (scheduler/create-report-in-flow {"opts"       {"email" "user@akvo.org"}
                                                "baseURL"    "http://foobar"
-                                               "exportType" "GEOSHAPE"
-                                               "flowId"     "id-returned-by-flow"}
-                                              {:report-path "some awesome path"})
-             {:method :put
-              :url    "http://foobar/reports/id-returned-by-flow"
-              :body   {:report {:state      "FINISHED_SUCCESS"
+                                               "exportType" "GEOSHAPE"})
+             {:method :post
+              :url    "http://foobar/reports"
+              :body   {:report {:state      "IN_PROGRESS"
                                 :user       "user@akvo.org"
-                                :reportType "GEOSHAPE"
-                                :filename   "some awesome path"}}})))
+                                :reportType "GEOSHAPE"}}})))
 
     (testing "finish request"
+      (are [report-result expected-body]
+        (= (scheduler/finish-report-in-flow {"opts"       {"email" "user@akvo.org"}
+                                             "baseURL"    "http://foobar"
+                                             "exportType" "GEOSHAPE"
+                                             "flowId"     "id-returned-by-flow"}
+                                            report-result)
+           {:method :put
+            :url    "http://foobar/reports/id-returned-by-flow"
+            :body   {:report (merge {:user       "user@akvo.org"
+                                     :reportType "GEOSHAPE"}
+                                    expected-body)}})
 
-      (testing "success"
-        (is (= (scheduler/finish-report-in-flow {"opts"       {"email" "user@akvo.org"}
-                                                 "baseURL"    "http://foobar"
-                                                 "exportType" "GEOSHAPE"
-                                                 "flowId"     "id-returned-by-flow"}
-                                                {:report-path "some awesome path"})
-               {:method :put
-                :url    "http://foobar/reports/id-returned-by-flow"
-                :body   {:report {:state      "FINISHED_SUCCESS"
-                                  :user       "user@akvo.org"
-                                  :reportType "GEOSHAPE"
-                                  :filename   "some awesome path"}}})))
+        {:report-path "some awesome path"} {:state    "FINISHED_SUCCESS"
+                                            :filename "some awesome path"}
 
-      (testing "error"
-        (is (= (scheduler/finish-report-in-flow {"opts"       {"email" "user@akvo.org"}
-                                                 "baseURL"    "http://foobar"
-                                                 "exportType" "GEOSHAPE"
-                                                 "flowId"     "id-returned-by-flow"}
-                                                {:report-path "INVALID_PATH"})
-               {:method :put
-                :url    "http://foobar/reports/id-returned-by-flow"
-                :body   {:report {:state      "FINISHED_ERROR"
-                                  :user       "user@akvo.org"
-                                  :reportType "GEOSHAPE"
-                                  :message    "Error generating report"}}})))
+        {:report-path "INVALID_PATH"} {:state   "FINISHED_ERROR"
+                                       :message "Error generating report"}
 
-      (testing "exception"
-        (is (= (scheduler/finish-report-in-flow {"opts"       {"email" "user@akvo.org"}
-                                                 "baseURL"    "http://foobar"
-                                                 "exportType" "GEOSHAPE"
-                                                 "flowId"     "id-returned-by-flow"}
-                                                {:exception (RuntimeException. "Something very bad")})
-               {:method :put
-                :url    "http://foobar/reports/id-returned-by-flow"
-                :body   {:report {:state      "FINISHED_ERROR"
-                                  :user       "user@akvo.org"
-                                  :reportType "GEOSHAPE"
-                                  :message    "Something very bad"}}})))
-      ))
+        {:exception (RuntimeException. "Something very bad")} {:state   "FINISHED_ERROR"
+                                                               :message "Something very bad"})))
   )
