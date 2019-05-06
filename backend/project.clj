3d36340e05696161bdbc207385a90299d3b7f1cd
stellanl <stellanl@akvo.org>
2019-05-06 19:36:05 +0200

    [#171]Bump jar version to tagged Squishy Squash release.

--- backend/project.clj
+++ backend/project.clj
@@ -33,7 +33,7 @@
                  [com.google.gdata/core "1.47.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  ; Akvo FLOW dependencies
-                 [org.akvo.flow/akvo-flow "v1.9.45-43-g0fbde6f" :classifier "classes"]
+                 [org.akvo.flow/akvo-flow "v1.9.46" :classifier "classes"]
                  [org.json/json "20090211"]
                  [org.codehaus.jackson/jackson-mapper-asl "1.9.13"]
                  ; Configuration and Stats
