c8d1b889337693ba2915337c7163334c81ced785
Valeria Rogatchevskikh <valeria@akvo.org>
2019-12-18 09:18:30 +0100

    Update project.clj
    update flow-services to use latest flow version

--- backend/project.clj
+++ backend/project.clj
@@ -34,7 +34,7 @@
                  [com.google.gdata/core "1.47.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  ; Akvo FLOW dependencies
-                 [org.akvo.flow/akvo-flow "v1.10-58-g867c78e9" :classifier "classes"]
+                 [org.akvo.flow/akvo-flow "v1.10.1.1-5-g8078a337" :classifier "classes"]
                  [org.json/json "20090211"]
                  [org.codehaus.jackson/jackson-mapper-asl "1.9.13"]
                  ; Configuration and Stats
