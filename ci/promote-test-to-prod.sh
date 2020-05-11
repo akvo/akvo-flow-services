f23d253c1cdcdef5abb0f502c72d00be7311d9d6
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-12 00:23:00 +0200

    [#288] Reuse devops container

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,47 +1,5 @@
 #!/usr/bin/env bash
 
-set -u
-
-function log {
-   echo "$(date +"%T") - INFO - $*"
-}
-
-PREVIOUS_CONTEXT=$(kubectl config current-context)
-
-function switch_back () {
-    log "Switching k8s context back to ${PREVIOUS_CONTEXT}"
-    kubectl config use-context "${PREVIOUS_CONTEXT}"
-}
-
-function read_version () {
-    CLUSTER=$1
-    log "Reading ${CLUSTER} version"
-    log "running: gcloud container clusters get-credentials ${CLUSTER} --zone europe-west1-d --project akvo-lumen"
-    if ! gcloud container clusters get-credentials "${CLUSTER}" --zone europe-west1-d --project akvo-lumen; then
-        log "Could not change context to ${CLUSTER}. Nothing done."
-        switch_back
-        exit 3
-    fi
-
-    VERSION=$(kubectl get deployments akvo-flow-services -o jsonpath="{@.spec.template.metadata.labels['akvo-flow-services-version']}")
-}
-
-read_version "test"
-TEST_VERSION=$VERSION
-
-read_version "production"
-PROD_VERSION=$VERSION
-
-log "Deployed test version is $TEST_VERSION"
-log "Deployed prod version is $PROD_VERSION"
-log "See https://github.com/akvo/akvo-flow-services/compare/$PROD_VERSION..$TEST_VERSION"
-
-TAG_NAME="promote-$(date +"%Y%m%d-%H%M%S")"
-
-log "To deploy, run: "
-echo "----------------------------------------------"
-echo "git tag $TAG_NAME $TEST_VERSION"
-echo "git push origin $TAG_NAME"
-echo "----------------------------------------------"
-
-switch_back
+docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/project \
+  -it akvo/akvo-devops:20200511.221343.5c060a8 \
+  promote-test-to-prod.sh akvo-flow-services akvo-flow-services-version akvo-flow-services
