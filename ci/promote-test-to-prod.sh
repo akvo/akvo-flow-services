d9823c88367502425afedfb6fa416b8f09a35b31
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-12 09:31:33 +0200

    [#288] Fix mount path

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/project \
+docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
   -it akvo/akvo-devops:20200511.221343.5c060a8 \
   promote-test-to-prod.sh akvo-flow-services akvo-flow-services-version akvo-flow-services
