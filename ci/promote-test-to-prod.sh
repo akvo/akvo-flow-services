6c75e0f3104240fb712a28c23bbc761994050342
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-13 09:19:52 +0200

    [#288] Latest version of promote script

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200512.075913.4b59be2 \
+  -it akvo/akvo-devops:20200512.132847.78aa475 \
   promote-test-to-prod.sh akvo-flow-services akvo-flow-services-version akvo-flow-services
