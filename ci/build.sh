08d9802fdc2ec807e958e799bd2a4f675cf70085
Iván Perdomo <ivan@perdomo.me>
2020-05-22 07:53:14 +0200

    [#290] Adds authn/authz to /sign endpoint via nginx

--- ci/build.sh
+++ ci/build.sh
@@ -20,8 +20,35 @@ log Running backend test and uberjar
 docker run -v "${HOME}/.m2:/root/.m2" -v "$(pwd)/backend:/app" akvo-flow-services-dev:develop lein 'do' test, uberjar
 
 log Building production container
-docker build --rm=false -t "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services:${TRAVIS_COMMIT}" ./backend
-docker tag "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services:${TRAVIS_COMMIT}" "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services:develop"
+docker build --rm=false \
+       -t "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services:${TRAVIS_COMMIT}" \
+       -t "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services:develop" \
+       -t "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services:latest" \
+       ./backend
+
+log Building proxy and checking container
+(
+    cd nginx
+    docker build \
+	   -t "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services-proxy:latest" \
+	   -t "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services-proxy:${TRAVIS_COMMIT}" .
+
+    docker run \
+	   --rm \
+	   --entrypoint /usr/local/openresty/bin/openresty \
+	   "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services-proxy" \
+	   -t -c /usr/local/openresty/nginx/conf/nginx.conf
+
+    docker run \
+	   --rm \
+	   --entrypoint /usr/local/openresty/bin/openresty \
+	   "eu.gcr.io/${PROJECT_NAME}/akvo-flow-services-proxy" \
+	   -t -c /usr/local/openresty/nginx/conf/nginx-test.conf
+
+    docker-compose up -d
+    docker-compose exec testnetwork /bin/sh -c 'cd /usr/local/src/ && ./entrypoint.sh ./test-auth.sh'
+    docker-compose down -v
+)
 
 log Starting docker compose env
 docker-compose -p akvo-flow-ci -f docker-compose.yml -f docker-compose.ci.yml up -d --build
