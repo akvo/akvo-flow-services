b3f4a1626e78269b80505d31ab09bc8e44b2dd72
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-10-26 15:09:20 +0100

    [#296] See what curl is complaining about

--- nginx/test-auth.sh
+++ nginx/test-auth.sh
@@ -4,6 +4,7 @@ set -eu
 
 wait4ports nginx=tcp://localhost:8082 upstream=tcp://localhost:3000
 
+curl --request GET --url "http://localhost:8082/"
 curl --silent --request GET --url "http://localhost:8082/" | grep 'OK'
 curl --silent --request GET --url "http://localhost:8082/invalidate" | grep 'OK'
 curl --verbose "http://localhost:8082/sign?instance=akvoflow-uat1" 2>&1 | grep 'HTTP.*401'
