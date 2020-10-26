c4caeefa904611ad58c127711ebf104704648910
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-10-26 17:03:32 +0100

    [#296] Log Auth0 response if there is no token
    It is failing intermittently

--- nginx/api.sh
+++ nginx/api.sh
@@ -7,14 +7,22 @@ function log {
 }
 
 log "Getting Auth0 token"
-token=$(curl --silent \
+auth0_response=$(curl --silent \
 	     --data "client_id=0C1NQcW6mMWyvvOOZ6Dd5P6hJrjSbwPL" \
 	     --data "username=${AUTH0_USER}" \
 	     --data "password=${AUTH0_PASSWORD}" \
 	     --data "grant_type=password" \
 	     --data "scope=openid email" \
-	     --url "https://akvotest.eu.auth0.com/oauth/token" \
-	    | jq -M -r .id_token)
+	     --url "https://akvotest.eu.auth0.com/oauth/token")
+
+token=$(echo "${auth0_response}" \
+	    | jq -M -r .id_token || "")
+
+if [[ -z "$token" ]]; then
+  log "No token found in Auth0 response:"
+  echo "$auth0_response"
+  exit 1
+fi
 
 log "Auth0 token: $(echo token | cut -c-6)"
 URL="${1}"
