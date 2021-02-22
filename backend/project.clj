7dea9034fe6369d00decccda0b1f8ec216dbb102
Valeria Rogatchevskikh <valeria@akvo.org>
2021-02-22 10:28:18 +0100

    Update project.clj

--- backend/project.clj
+++ backend/project.clj
@@ -34,7 +34,7 @@
                  [com.google.gdata/core "1.47.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  ; Akvo FLOW dependencies
-                 [org.akvo.flow/akvo-flow "20210114-103854.403c49fc" :classifier "classes"]
+                 [org.akvo.flow/akvo-flow "20210221-083504.44c3dad5" :classifier "classes"]
                  [org.json/json "20090211"]
 
                  ;; Override Jackson version from [cheshire "5.9.0"]
